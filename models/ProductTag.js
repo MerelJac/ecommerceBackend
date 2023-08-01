502a36c6ab3959881c820f40ecdc012ccc98e100
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-01 06:25:28 -0700

    debug sequelize in productTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connections');
+const { route } = require('../routes');
 
 class ProductTag extends Model {};
 
@@ -22,4 +23,12 @@ ProductTag.init({
         type: DataTypes.INTEGER
         // references tag[id]
     }
+}, {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+    modelName: 'product_tag'
 });
+
+module.exports = ProductTag;
